6659409aead613a621db7c9853b44255589c501e
Kryle8 <151596121+Kryle8@users.noreply.github.com>
2023-11-22 12:37:21 +0800

    Event models

--- src/com/acrosys/models/Event.java
+++ src/com/acrosys/models/Event.java
@@ -9,5 +9,60 @@ package com.acrosys.models;
  * @author kryle
  */
 public class Event {
+    private int id;
+    private String controlno;
+    private String eventname;
+    private String client_name;
+    private int client_age;
+    private String client_gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getControlno() {
+        return controlno;
+    }
+
+    public void setControlno(String controlno) {
+        this.controlno = controlno;
+    }
+
+    public String getEventname() {
+        return eventname;
+    }
+
+    public void setEventname(String eventname) {
+        this.eventname = eventname;
+    }
+
+    public String getClient_name() {
+        return client_name;
+    }
+
+    public void setClient_name(String client_name) {
+        this.client_name = client_name;
+    }
+
+    public int getClient_age() {
+        return client_age;
+    }
+
+    public void setClient_age(int client_age) {
+        this.client_age = client_age;
+    }
+
+    public String getClient_gender() {
+        return client_gender;
+    }
+
+    public void setClient_gender(String client_gender) {
+        this.client_gender = client_gender;
+    }
+    
     
 }
