72e4bd47e2a9b0085498f4e0c9392bca434e72d4
tenshi-janai <151596954+tenshi-janai@users.noreply.github.com>
2023-11-22 17:31:33 +0800

    EvenController

--- src/com/acrosys/controllers/EventController.java
+++ src/com/acrosys/controllers/EventController.java
@@ -5,11 +5,14 @@
 package com.acrosys.controllers;
 
 import com.acrosys.interfaces.EventInterface;
+import com.acrosys.models.Attendee;
 import com.acrosys.models.Event;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -27,7 +30,7 @@ public class EventController implements EventInterface {
             statement.setString(1, event_code);                 
             ResultSet rs = statement.executeQuery();                    
             
-            while (rs.next()) {                      
+            while (rs.next()) {              
                 Event event = new Event();                        
                 
                 event.setName(rs.getString("name"));                      
@@ -45,6 +48,35 @@ public class EventController implements EventInterface {
             System.out.println("Logs: " + e.getMessage());          
         }              
         return null;
+    }
+
+    @Override
+    public List<Event> eventList() {
+        List<Event> list = new ArrayList<>();              
+        try {                  
+            Connection conn = DatabaseConnection.getConnection();                  
+            String sql = "SELECT * FROM tbl_event ORDER BY name";                  
+            PreparedStatement statement = conn.prepareStatement(sql);                
+            ResultSet rs = statement.executeQuery();
+            
+            while (rs.next()) {              
+                Event event = new Event();                        
+                
+                event.setName(rs.getString("name"));                      
+                event.setCode(rs.getString("event_code"));                      
+                event.setDescription(rs.getString("description"));                      
+                event.setDate(rs.getTimestamp("date").toLocalDateTime());                      
+                event.setTotal_attendees(rs.getInt("total_attendees"));                      
+                event.setChecked_in(rs.getInt("checked_in"));                   
+                event.setPending(rs.getInt("pending"));
+                list.add(event);
+            }          
+        } catch (SQLException e) {              
+            JOptionPane.showMessageDialog(null,"Unable to get list of Events. Please see logs.","Save Error", JOptionPane.ERROR_MESSAGE);                            
+            
+            System.out.println("Logs: " + e.getMessage());          
+        }              
+        return list;
     }
     
 }
