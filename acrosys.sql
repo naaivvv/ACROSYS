81810109b0f6597ee7fa058112c995de4d535743
tenshi-janai <151596954+tenshi-janai@users.noreply.github.com>
2023-11-23 14:53:21 +0800

    Update acrosys.sql

--- acrosys.sql
+++ acrosys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2023 at 09:06 AM
+-- Generation Time: Nov 23, 2023 at 07:52 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -33,10 +33,63 @@ CREATE TABLE `tbl_attendees` (
   `client_name` varchar(50) NOT NULL,
   `client_age` int(3) NOT NULL,
   `client_gender` varchar(6) NOT NULL,
-  `control_number` varchar(10) NOT NULL,
+  `control_number` varchar(20) DEFAULT NULL,
+  `isChecked_in` tinyint(1) NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_attendees`
+--
+
+INSERT INTO `tbl_attendees` (`id`, `event_code`, `client_name`, `client_age`, `client_gender`, `control_number`, `isChecked_in`, `created_at`, `updated_at`) VALUES
+(1, 'DEV', 'KARLRYLE', 19, 'FEMALE', 'DEV0000000001', 0, '2023-11-23 06:38:17', '2023-11-23 06:38:17'),
+(2, 'GIE', 'GEANNE', 19, 'FEMALE', 'GIE0000000002', 0, '2023-11-23 06:47:17', '2023-11-23 06:47:17');
+
+--
+-- Triggers `tbl_attendees`
+--
+DELIMITER $$
+CREATE TRIGGER `count_checked` AFTER UPDATE ON `tbl_attendees` FOR EACH ROW BEGIN
+	IF NEW.isChecked_in THEN
+    	UPDATE tbl_event SET checked_in = checked_in + 1 WHERE event_code = NEW.event_code;
+    END IF;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `count_total` AFTER INSERT ON `tbl_attendees` FOR EACH ROW BEGIN
+	UPDATE tbl_event SET total_attendees = total_attendees + 1 WHERE event_code = NEW.event_code;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `getID` BEFORE INSERT ON `tbl_attendees` FOR EACH ROW BEGIN
+	INSERT INTO tbl_ctrln VALUES (NULL);
+    SET NEW.control_number = CONCAT(NEW.event_code, 
+LPAD(LAST_INSERT_ID(), 10, "0"));
+	END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ctrln`
+--
+
+CREATE TABLE `tbl_ctrln` (
+  `id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_ctrln`
+--
+
+INSERT INTO `tbl_ctrln` (`id`) VALUES
+(1),
+(2);
 
 -- --------------------------------------------------------
 
@@ -50,12 +103,20 @@ CREATE TABLE `tbl_event` (
   `event_code` varchar(5) NOT NULL,
   `description` varchar(300) NOT NULL,
   `date` datetime NOT NULL,
-  `total_attendees` int(11) NOT NULL,
-  `checked_in` int(11) NOT NULL,
-  `pending` int(11) NOT NULL,
+  `total_attendees` int(11) NOT NULL DEFAULT 0,
+  `checked_in` int(11) NOT NULL DEFAULT 0,
+  `pending` int(11) NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_event`
+--
+
+INSERT INTO `tbl_event` (`id`, `name`, `event_code`, `description`, `date`, `total_attendees`, `checked_in`, `pending`, `created_at`, `updated_at`) VALUES
+(1, 'DEVFEST 2023', 'DEV', '...', '2023-11-23 07:37:43', 1, 0, 0, '2023-11-23 06:37:57', '2023-11-23 06:38:17'),
+(2, 'Birthday ni Giezhia', 'GIE', '...', '2023-11-23 07:46:13', 1, 0, 0, '2023-11-23 06:46:46', '2023-11-23 06:47:17');
 
 -- --------------------------------------------------------
 
@@ -73,6 +134,13 @@ CREATE TABLE `tbl_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `permission`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'password', 'Admin', '2023-11-22 23:11:21', '2023-11-23 06:32:49');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -80,6 +148,12 @@ CREATE TABLE `tbl_user` (
 -- Indexes for table `tbl_attendees`
 --
 ALTER TABLE `tbl_attendees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_ctrln`
+--
+ALTER TABLE `tbl_ctrln`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -102,19 +176,25 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_attendees`
 --
 ALTER TABLE `tbl_attendees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_ctrln`
+--
+ALTER TABLE `tbl_ctrln`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_event`
 --
 ALTER TABLE `tbl_event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
